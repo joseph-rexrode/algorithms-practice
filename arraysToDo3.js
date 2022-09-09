c4956d83ca66536b4ba96add48736153083aa49d
jrexrode <jrexrode@me.com>
2022-09-09 13:24:20 -0400

    3 more problems completed

--- arraysToDo3.js
+++ arraysToDo3.js
@@ -36,3 +36,73 @@ console.log(secondToLast([1]));
 console.log(secondToLast(['a', 'b']));
 
 
+//// SECOND LARGEST ////
+
+function secondLargest(arr) {
+    if (arr.length <= 1) {
+        return null;
+    }
+
+    var largest = arr[0];
+    var nextLargest;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            nextLargest = largest;
+            largest = arr[i];
+        }
+        else {
+            if (arr[i] > nextLargest || nextLargest == null) {
+                nextLargest = arr[i];
+            }
+        }
+    }
+    return nextLargest;
+}
+
+console.log(secondLargest([1,2,3,4,5]));
+console.log(secondLargest([-2, 6, 19, 12, 13]));
+console.log(secondLargest([1]));
+
+
+//// NTH-TO-LAST ////
+
+function nthToLast(arr, n) {
+    if (arr.length - n < 0) {
+        return null;
+    }
+
+    return arr[arr.length - n];
+}
+
+console.log(nthToLast([5,2,3,6,4,9,7], 3));
+
+
+//// NTH-LARGEST ////
+
+function nthLargest(arr, n) {
+    if (arr.length - n < 0) {
+        return null;
+    }
+
+    for (var i = 0; i < arr.length; i++) {
+        var count = 0;
+        for (var j = 0; j < arr.length; j++) {
+            if (i == j) {
+                continue;
+            }
+            if (arr[i] >= arr[j]) {
+                count++;
+            }
+        }
+        if (count == arr.length - n) {
+            return arr[i];
+        }
+    }
+
+    return null;
+}
+
+console.log(nthLargest([1,2,3,4,5], 4));
+
+
